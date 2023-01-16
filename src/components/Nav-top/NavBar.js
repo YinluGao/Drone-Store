133c25a9d990b0deee458e4f397d7b6875dddbf1
Yinlu ( Claire) Gao <112586161+YinluGao@users.noreply.github.com>
2023-01-16 13:54:21 +0100

    Add files via upload

---
+++ src/components/Nav-top/NavBar.js
@@ -0,0 +1,29 @@
+import { React } from "react";
+import Logo from "./Logo";
+import SearchBar from "./SearchBar";
+import NavBarIcons from "./NavBarIcons";
+import SignIn from "./SignIn";
+import NavBarCSS from "./NavBar.module.css";
+
+const NavBar = ({
+	toggleCart,
+	cartLen,
+	toggleFavorites,
+	favoritesLen,
+	changeFilter,
+}) => {
+	return (
+		<nav className={NavBarCSS.navbarContainer}>
+			<Logo />
+			<SearchBar changeFilter={changeFilter} />
+			<NavBarIcons
+				toggleCart={toggleCart}
+				cartLen={cartLen}
+				toggleFavorites={toggleFavorites}
+				favoritesLen={favoritesLen}
+			/>
+			{/* <SignIn /> */}
+		</nav>
+	);
+};
+export default NavBar;
